a8c1e5113db3dcce5b28b493ae6dd48ad7cc0297
RohitSingh <fgroup.rohit@gmail.com>
2020-05-19 02:02:01 +0530

    EduGain

---
+++ app/src/main/java/com/edugainnow/edugain/viewpagerslider/fragments/ScreenSlidePageFragment.java
@@ -0,0 +1,20 @@
+package com.edugainnow.edugain.viewpagerslider.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edugainnow.edugain.R;
+
+import androidx.fragment.app.Fragment;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+    }
+}
