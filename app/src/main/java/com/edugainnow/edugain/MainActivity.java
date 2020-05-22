0d1300c7c8f3c7efccf4adab64173b640fe94875
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-05-22 15:04:31 +0530

    EduGain

--- app/src/main/java/com/edugainnow/edugain/MainActivity.java
+++ app/src/main/java/com/edugainnow/edugain/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setTheme(R.style.AppTheme_BottomNavigation);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
